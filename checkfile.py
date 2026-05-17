306c4ff9ae140b65e1b3d8daad2872f59a706f6b
tingcangnirelamor <tingcang.nirelamor@gmail.com>
2026-05-17 04:05:31 +0000

    deposit

--- checkfile.py
+++ checkfile.py
@@ -7,5 +7,27 @@ except:
 
     with open("balance.txt", "w") as file:
         file.write(str(balance))
+                
+def deposit():
+    global balance
 
+    amount = entry.get()
+
+    try:
+        amount = float(amount)
+
+        if amount <= 0:
+            messagebox.showerror("Error", "Invalid amount")
+            return
+
+        balance += amount
+
+        update_balance()
+
+        save_transaction(f"Deposited: {amount}")
+
+        messagebox.showinfo("Success", "Deposit successful")
+
+    except:
+        messagebox.showerror("Error", "Enter numbers only")
         
